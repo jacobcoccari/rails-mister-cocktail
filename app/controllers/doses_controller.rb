5f8c867208120d5a14940e2361f242c595f21093
jacob <jacob.coccari@gmail.com>
2019-03-07 19:09:19 +0100

    skrt

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,19 +2,23 @@ class DosesController < ApplicationController
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
-    @ingredients = []
-    Ingredient.all.each { |ingredient| @ingredients << ingredient.name }
+    # @ingredients = []
+    # Ingredient.all.each { |ingredient| @ingredients << ingredient.name }
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   private
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
